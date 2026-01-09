8fd6c3de727843fc79c34e9ff4723cdb457571ca
Rishi-Bhati <bhatirishi5@gmail.com>
2026-01-09 12:38:43 +0530

    changed secret key and some settings

--- core/settings.py
+++ core/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#zh%)qypz4t5uc_9%dy=y!f4t0*!x$qz^54kt&p#i(z1lesa8$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = "accounts.User"
 
