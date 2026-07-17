aab38601ea9ee73fcde629759294f6ff87328048
Rishi-Bhati <bhatirishi5@gmail.com>
2026-07-17 23:45:53 +0530

    config: expose EMAIL_PORT, EMAIL_USE_TLS, and EMAIL_USE_SSL to environment variables

--- core/settings.py
+++ core/settings.py
@@ -255,9 +255,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Configuration (Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() in ('true', '1', 'yes')
 EMAIL_HOST_USER = os.getenv('MAIL_ID', '')
 EMAIL_HOST_PASSWORD = os.getenv('MAIL_APP_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('MAIL_ID', 'noreply@reportary.com')
